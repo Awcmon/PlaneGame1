b14515e84f529403f155a10fbbd1f400c760b795
Awcmon <adcarbine@yahoo.com>
2019-03-31 21:52:43 -0700

    disable resizing of window

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,18 @@
 
 //========================================================================
 int main( ){
+	/*
 	ofSetupOpenGL(1280,960,OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
 	ofRunApp(new ofApp());
+	*/
 
+	ofGLFWWindowSettings settings;
+	settings.resizable = false;
+	settings.setSize(1280, 960);
+	ofCreateWindow(settings);
+	return ofRunApp(new ofApp);
 }
